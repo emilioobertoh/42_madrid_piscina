e40fa1043a3f09e9650a4f286f7afdf1efd5ab49
emilioobertoh <obertoe.1996@gmail.com>
2024-05-28 12:07:17 +0200

    Added c01 ex01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eoberto- <eoberto-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 12:00:28 by eoberto-          #+#    #+#             */
+/*   Updated: 2024/05/28 12:06:30 by eoberto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int a;
+	int *ptr1, **ptr2, ***ptr3, ****ptr4, *****ptr5, ******ptr6, *******ptr7, ********ptr8, *********ptr9;
+
+	a = 21;
+	ptr1 = &a;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	printf("Avant (21): %d\n", a);
+	ft_ultimate_ft(ptr9);
+	printf("Apres (42): %d\n", a);
+}
