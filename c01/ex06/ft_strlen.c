b471f2dbc9e5de0f478747272c0c48e982da649c
Emilio Josue Oberto Hernandez <eoberto-@c2r13s2.42madrid.com>
2024-05-29 11:59:24 +0200

    Added ex06

---
+++ c01/ex06/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eoberto- <eoberto-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 11:38:48 by eoberto-          #+#    #+#             */
+/*   Updated: 2024/05/29 11:58:58 by eoberto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*str;
+
+	str = "Hola!!!!!!";
+	printf("return %d\n", ft_strlen(str));
+}
+*/
