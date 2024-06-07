f4825487fe9590afe6ff9cae3b45c6793cb9372a
emilioobertoh <obertoe.1996@gmail.com>
2024-06-07 08:47:12 +0200

    Corrected norm in c03 ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eoberto- <eoberto-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/06 00:43:50 by eoberto-          #+#    #+#             */
-/*   Updated: 2024/06/07 00:51:58 by skodiak          ###   ########.fr       */
+/*   Created: 2024/06/07 08:27:57 by eoberto-          #+#    #+#             */
+/*   Updated: 2024/06/07 08:43:45 by skodiak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,44 @@ char	*ft_strstr(char *str, char *to_find)
 	int	mismatch;
 
 	if (*(to_find) == '\0')
-	{
-	    return (str);
-	}
+		return (str);
 	while (*(str) != '\0')
 	{
-	    if (*(str) == *(to_find))
-	    {
-		idx = 0;
-		mismatch = 0;
-		while (*(to_find + idx) != '\0')
+		if (*(str) == *(to_find))
 		{
-		    if (*(str + idx) != *(to_find + idx))
-		    {
-			mismatch = 1;
-		    }
-		    idx++;
+			idx = 0;
+			mismatch = 0;
+			while (*(to_find + idx) != '\0')
+			{
+				if (*(str + idx) != *(to_find + idx))
+					mismatch = 1;
+				idx++;
+			}
+			if (mismatch == 0)
+				return (str);
 		}
-		if (mismatch == 0)
-		{
-		    return (str);
-		}
-	    }
-	    str++;
+		str++;
 	}
 	return (0);
 }
 /*
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-    char str1[] = "Hello, world!";
-    char to_find1[] = "hola";
-    char *result1;
+	char	str1[] = "Hello, world!";
+	char	to_find1[] = "wenas";
+	char	*result1;
 
-    result1 = ft_strstr(str1, to_find1);
-    if (result1)
-        printf("Found '%s' in '%s': '%s'\n", to_find1, str1, result1);
-    else
-        printf("'%s' not found in '%s'\n", to_find1, str1);
-
-    return 0;
+	result1 = ft_strstr(str1, to_find1);
+	if (result1)
+	{
+		printf("Found '%s' in '%s'\n", to_find1, str1);
+	}
+	else
+	{
+		printf("'%s' not found in '%s'\n", to_find1, str1);
+	}
+	return (0);
 }
 */
